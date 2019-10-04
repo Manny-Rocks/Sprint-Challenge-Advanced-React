b3ec745065908a5bc5ea2493c062c15a8bd2c8bb
Emmanuel Tanifum <etantanifum@gmail.com>
2019-10-04 12:04:26 -0400

    styles added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import NavBar from './components/NavBar'
+import SoccerCardsMaker from './components/SoccerCardsMaker'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends React.Component {
+  constructor() {  
+    super()
+    this.state = { soccerPlayers: [] }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/players`)
+      .then( (res) => {
+        console.log(res)
+        this.setState( {soccerPlayers: res.data})
+        console.log(this.state)
+      })
+  }
+  
+
+  render() {
+    return (
+      <>
+       <NavBar />
+       <div className="rex">
+      {this.state.soccerPlayers.map( (player) => {
+        return <SoccerCardsMaker key={player.id} player={player} />
+      })}
+      </div>
+      </>
+    )
+  }
 }
-
-export default App;
