873b214987a33a412ae5af441f5b8315fd846f3d
Emmanuel Tanifum <etantanifum@gmail.com>
2019-10-04 12:58:16 -0400

    made some test...2 test pass

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NavBar from './components/NavBar';
+import * as rtl from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('NavBar renders without crashing', () => {
+  const { getByText } = render(<NavBar />)
+  getByText("MANNY'S SOCCER CARDS")
+})
+
